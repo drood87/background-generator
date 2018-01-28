f85651bfcf34243d9ec59ae84346041faa46bda7
drood87 <dbernstein1987@hotmail.com>
2018-01-28 12:17:02 +1100

    added new feature for 3 linear background colors

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ const css = document.querySelector("h3");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 
+const color3 = document.querySelector(".color3");
+
 const body = document.getElementById("gradient");
 
 const colorRandom1 = '#' +(Math.floor(Math.random()*16777215).toString(16));
 const colorRandom2 = '#' +(Math.floor(Math.random()*16777215).toString(16));
+const colorRandom3 = '#' +(Math.floor(Math.random()*16777215).toString(16));
 
 const randomButton = document.getElementById("random");
-// const switchButton = document.getElementbyId("switch");
+const switchButton = document.getElementById("switch");
 
 document.querySelector('input[class="color1"]').value = colorRandom1;
 document.querySelector('input[class="color2"]').value = colorRandom2;
@@ -44,6 +47,12 @@ function switchTo3RandomColors() {
 
     body.style.background = `linear-gradient(to right, ${colorRandom1}, ${colorRandom2}, ${colorRandom3})`;
 
+    document.querySelector('input[class="color1"]').value = colorRandom1;
+    document.querySelector('input[class="color2"]').value = colorRandom2;
+    document.querySelector('input[class="color3"]').value = colorRandom3;
+
+    css.textContent = body.style.background + ";";
+
 }
 
 // creating function to change the color on user input
@@ -56,6 +65,8 @@ function switchTo3RandomColors() {
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+color3.addEventListener("input", setGradient);
+
 randomButton.addEventListener("click", generateRandomColor);
 switchButton.addEventListener("click", switchTo3RandomColors);
 
